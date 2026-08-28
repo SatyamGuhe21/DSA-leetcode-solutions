736ffe2f2d2170ad0d525f234cba6e5ab5bd9a1c
Satyam Guhe <121849340+SatyamGuhe21@users.noreply.github.com>
2026-08-28 09:46:29 +0530

    Time: 1 ms (100.00%) | Memory: 47.1 MB (18.44%) - LeetSync

---
+++ 2684-determine-the-winner-of-a-bowling-game/determine-the-winner-of-a-bowling-game.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int isWinner(int[] player1, int[] player2) {
+
+        int score1 = 0;
+        int score2 = 0;
+
+        for (int i = 0; i < player1.length; i++) {
+
+            if (i > 0 && player1[i - 1] == 10 ||
+                i > 1 && player1[i - 2] == 10) {
+                score1 += player1[i] * 2;
+            } else {
+                score1 += player1[i];
+            }
+
+            if (i > 0 && player2[i - 1] == 10 ||
+                i > 1 && player2[i - 2] == 10) {
+                score2 += player2[i] * 2;
+            } else {
+                score2 += player2[i];
+            }
+        }
+
+        if (score1 > score2) {
+            return 1;
+        } else if (score2 > score1) {
+            return 2;
+        }
+
+        return 0;
+    }
+}
