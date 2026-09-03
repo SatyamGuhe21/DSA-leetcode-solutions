8c5d63d04d1ef94814541f34191dc094bf4c3c1b
Satyam Guhe <121849340+SatyamGuhe21@users.noreply.github.com>
2026-09-03 09:48:38 +0530

    Time: 3 ms (76.94%) | Memory: 46.7 MB (36.95%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,35 +1,42 @@
 class Solution {
     public int romanToInt(String s) {
-        
-        HashMap<Character, Integer>  map =  new HashMap<>(); 
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
+
         int total = 0;
 
-        for(int i = 0; i < s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
 
-            int current = map.get(s.charAt(i));
+            int current = value(s.charAt(i));
 
+            if (i + 1 < s.length()) {
 
-            if(i+1 < s.length()){
-                int next = map.get(s.charAt(i+1));
+                int next = value(s.charAt(i + 1));
 
-                if(current < next){
+                if (current < next) {
                     total -= current;
+                } else {
+                    total += current;
                 }
-                else{
-                    total+= current;
-                }
-            }else{
+
+            } else {
                 total += current;
             }
         }
 
         return total;
+    }
+
+    public int value(char c) {
+
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+
+        return 0;
     }
 }
