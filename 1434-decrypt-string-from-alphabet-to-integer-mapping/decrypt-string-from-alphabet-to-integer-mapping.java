0ef2d7071a475755220be204228b784cdc2a47ae
Satyam Guhe <121849340+SatyamGuhe21@users.noreply.github.com>
2026-09-23 09:25:33 +0530

    Time: 1 ms (95.84%) | Memory: 42.8 MB (87.52%) - LeetSync

---
+++ 1434-decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String freqAlphabets(String s) {
+       StringBuilder result = new StringBuilder();
+
+        for(int i = 0; i < s.length(); i++){
+
+            if(i+2 <s.length() && s.charAt(i +2) == '#'){
+
+                int num = Integer.parseInt(s.substring(i, i+2));
+
+                result.append((char)('a' + num -1));
+
+                i+= 2;
+            }
+            else{
+                int num = s.charAt(i) - '0';
+                result.append((char)('a' + num - 1));
+            }
+        }
+         return result.toString();
+    }
+}
